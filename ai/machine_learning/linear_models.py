aa75d8622716a470ef2e66a9522769b6f42e3d83
srikar akkina <51265375+akkinasrikar@users.noreply.github.com>
2020-08-12 10:22:38 +0530

    Add files via upload

--- ai/machine_learning/linear_models.py
+++ ai/machine_learning/linear_models.py
@@ -8,11 +8,34 @@ class gradient_descent:
         for i in range(epochs):
             intercept=np.sum(np.subtract((np.dot(self.m,x_train.T)+self.c),y_train))
             slope=sum(np.dot(((np.dot(self.m,x_train.T)+self.c)-y_train),x_train))
-            #print(slope,intercept)
             self.c=self.c-np.dot(learning_rate,intercept)/l
             self.m=self.m-np.dot(learning_rate,slope)/l
-            #print(m,c)
-        #print(m,c)
+    def slope_intercept(self):
+        print(self.m,self.c)
+    def predict(self,x_test):
+        self.m=self.m.reshape(self.m.shape[1],self.m.shape[0])
+        result=np.dot(x_test,self.m)+self.c
+        return result
+
+class SGD:
+
+    def fit(self,x_train,y_train,learning_rate,epochs):
+        self.m=np.random.randn(1,x_train.shape[1])
+        self.c=0.5
+        l=len(x_train)
+        for i in range(epochs):
+            intercept=0
+            slope=0
+            for j in range(l):
+                ind=np.random.randint(l)
+                s=x_train[ind].reshape(x_train[ind].shape[0],1)
+                intercept=np.sum(np.subtract((np.dot(self.m,s)+self.c),y_train[ind]))
+                slope=sum(np.dot(((np.dot(self.m,s)+self.c)-y_train[ind]),s.T))
+            self.c=self.c-np.dot(learning_rate,intercept)/l
+            self.m=self.m-np.dot(learning_rate,slope)/l
+    def slope_intercept(self):
+        print(self.m,self.c) 
+               
     def predict(self,x_test):
         self.m=self.m.reshape(self.m.shape[1],self.m.shape[0])
         result=np.dot(x_test,self.m)+self.c
@@ -21,15 +44,17 @@ class gradient_descent:
 class Linear_regression:
 
     def fit(self,x_train,y_train):
-        x_new_train=np.c_[np.ones((len(x_train),1)),x_train]
-        self.weigts_and_intercept=np.linalg.pinv(x_new_train.T.dot(x_new_train)).dot(x_new_train.T).dot(y_train)
-        self.weights=self.weigts_and_intercept[1:]
-        self.intercept=self.weigts_and_intercept[0:1]
+        x_new=np.c_[np.ones((len(x_train),1)),x_train]
+        self.m_c=np.linalg.inv(x_new.T.dot(x_new)).dot(x_new.T).dot(y_train)
+        self.m=self.m_c[1:]
+        self.c=self.m_c[0:1]
 
     def predict(self,x_test):
         x_new_test=np.c_[np.ones((len(x_test),1)),x_test]
-        y_pred=x_new_test.dot(self.weigts_and_intercept)
+        y_pred=x_new_test.dot(self.m_c)
         return y_pred
+
+
 
 
         
